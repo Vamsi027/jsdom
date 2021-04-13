341d9688ac6a54a823a86901f5fa3d19fb0a932c
Vamsi027 <you@example.com>
2021-04-13 11:28:22 +0530

    js getElementbyclassname usage

--- dom.js
+++ dom.js
@@ -12,3 +12,17 @@ header.style.border='solid 3px #000'
 var addItem=document.getElementById('addItem')
 addItem.style.color='green'
 addItem.style.fontWeight='bold'
+
+var items=document.getElementsByClassName('list-group-item')
+console.log(items)
+items[1].textContent='Hello 2'
+items[1].style.fontWeight='bold'
+items[1].style.backgroundColor='yellow'
+items[2].textContent='Hello 3'
+items[2].style.fontWeight='bold'
+items[2].style.backgroundColor='green'
+
+for(var i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold'
+}
