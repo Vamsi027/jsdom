e6ee32bf2e23fc68f403996800d8a98cb29f641c
Vamsi027 <you@example.com>
2021-04-14 22:05:30 +0530

    delete and edit

--- dom.js
+++ dom.js
@@ -59,32 +59,78 @@
 // item3[0].style.color='blue'
 
 
-var itemList=document.querySelector('#items')
-itemList.parentElement.style.background='yellow'
+// var itemList=document.querySelector('#items')
+// itemList.parentElement.style.background='yellow'
 
-console.log(itemList.firstChild) //not useful with the current index.html
+// console.log(itemList.firstChild) //not useful with the current index.html
 
-itemList.firstElementChild.textContent='Hello 1'
-itemList.lastElementChild.textContent='Hello 5'
+// itemList.firstElementChild.textContent='Hello 1'
+// itemList.lastElementChild.textContent='Hello 5'
 
-console.log(itemList.nextSibling) //not useful with the current index.html
-console.log(itemList.nextElementSibling) //not useful with the current index.html
+// console.log(itemList.nextSibling) //not useful with the current index.html
+// console.log(itemList.nextElementSibling) //not useful with the current index.html
 
-console.log(itemList.previousSibling) //not useful with the current index.html
-itemList.previousElementSibling.style.color='green'
+// console.log(itemList.previousSibling) //not useful with the current index.html
+// itemList.previousElementSibling.style.color='green'
 
-var newDiv=document.createElement('div')
-newDiv.className='hello'
-newDiv.id='hello1'
-newDiv.setAttribute('title','Hello Div')
-var newDivtext=document.createTextNode('Hello World')
-newDiv.appendChild(newDivtext)
+// var newDiv=document.createElement('div')
+// newDiv.className='hello'
+// newDiv.id='hello1'
+// newDiv.setAttribute('title','Hello Div')
+// var newDivtext=document.createTextNode('Hello World')
+// newDiv.appendChild(newDivtext)
 
-var container=document.querySelector('header .container')
-var h1=document.querySelector('header h1')
+// var container=document.querySelector('header .container')
+// var h1=document.querySelector('header h1')
 
-container.insertBefore(newDiv,h1)
+// container.insertBefore(newDiv,h1)
 
-var container2=document.querySelector('.card-body')
-var l1=document.querySelector('#items')
-container2.insertBefore(newDiv,l1)
+// var container2=document.querySelector('.card-body')
+// var l1=document.querySelector('#items')
+// container2.insertBefore(newDiv,l1)
+
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');    
+
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+itemList.addEventListener('click', editItem);
+
+function addItem(e){
+  e.preventDefault();
+
+  var newItem = document.getElementById('item').value;
+
+  var li = document.createElement('li');
+  
+  li.className = 'list-group-item';
+  
+  li.appendChild(document.createTextNode(newItem));
+
+  
+  var deleteBtn = document.createElement('button');
+
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+
+  deleteBtn.appendChild(document.createTextNode('X'));
+
+  li.appendChild(deleteBtn);
+
+  itemList.appendChild(li);
+}
+
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+function editItem(e){
+    if(e.target.classList.contains('edit')){
+        var li = e.target.parentElement;
+        li.contentEditable = "true";
+      }
+  }
