32ff577080c798511d1dcdaa43c44e9e07910c47
Vamsi027 <you@example.com>
2021-04-14 18:57:38 +0530

    Creating Nodes And Modifying Dom

--- dom.js
+++ dom.js
@@ -36,24 +36,55 @@
 // diffItem1[4].textContent='Goodbye'
 // diffItem1[4].style.backgroundColor='yellow'
 
-var input=document.querySelector('input')
-input.value='Hello'
+// var input=document.querySelector('input')
+// input.value='Hello'
 
-var submit=document.querySelector('input[type=submit]')
-submit.value='Send'
+// var submit=document.querySelector('input[type=submit]')
+// submit.value='Send'
 
-var item=document.querySelector('.list-group-item:nth-child(2)')
-item.style.backgroundColor='green'
+// var item=document.querySelector('.list-group-item:nth-child(2)')
+// item.style.backgroundColor='green'
 
-var item2=document.querySelector('.list-group-item:nth-child(3)')
-item2.style.display='none'
+// var item2=document.querySelector('.list-group-item:nth-child(3)')
+// item2.style.display='none'
 
-//item3 is hidden at this pt but it is yellow as well
-var odd=document.querySelectorAll('li:nth-child(odd)')
-for(var i=0;i<odd.length;i++)
-{
-    odd[i].style.backgroundColor='yellow'
-}
+// //item3 is hidden at this pt but it is yellow as well
+// var odd=document.querySelectorAll('li:nth-child(odd)')
+// for(var i=0;i<odd.length;i++)
+// {
+//     odd[i].style.backgroundColor='yellow'
+// }
 
-var item3=document.querySelectorAll('li:nth-child(2)')
-item3[0].style.color='blue'
+// var item3=document.querySelectorAll('li:nth-child(2)')
+// item3[0].style.color='blue'
+
+
+var itemList=document.querySelector('#items')
+itemList.parentElement.style.background='yellow'
+
+console.log(itemList.firstChild) //not useful with the current index.html
+
+itemList.firstElementChild.textContent='Hello 1'
+itemList.lastElementChild.textContent='Hello 5'
+
+console.log(itemList.nextSibling) //not useful with the current index.html
+console.log(itemList.nextElementSibling) //not useful with the current index.html
+
+console.log(itemList.previousSibling) //not useful with the current index.html
+itemList.previousElementSibling.style.color='green'
+
+var newDiv=document.createElement('div')
+newDiv.className='hello'
+newDiv.id='hello1'
+newDiv.setAttribute('title','Hello Div')
+var newDivtext=document.createTextNode('Hello World')
+newDiv.appendChild(newDivtext)
+
+var container=document.querySelector('header .container')
+var h1=document.querySelector('header h1')
+
+container.insertBefore(newDiv,h1)
+
+var container2=document.querySelector('.card-body')
+var l1=document.querySelector('#items')
+container2.insertBefore(newDiv,l1)
